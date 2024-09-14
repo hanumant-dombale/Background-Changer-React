8caf72ceb2d8dd4bfd25b75808d6cbfd6f6e9f89
Hanumamt Dombale <hanumantdombale60@gmail.com>
2024-09-15 02:02:56 +0530

    Upload file in this repo

---
+++ README.md
@@ -0,0 +1,21 @@
+# Background-Changer-React
+
+Background Changer is a dynamic and user-friendly web application built with Vite and React,
+designed to effortlessly transform your browser's background.
+With a vast collection of stunning images, you can quickly and easily customize
+your browsing experience to match your mood or style.
+
+# Key Features:
+
+Real-time Updates: Instantly see the changes reflected on your browser's background as you choose different images.
+Favorites Functionality: Save your favorite backgrounds for quick access later.
+Search Functionality: Efficiently find the perfect image using our powerful search feature.
+
+# Technologies Used:
+
+Vite: A lightning-fast development server and build tool for modern web applications.
+React: A popular JavaScript library for building user interfaces.   
+CSS: For styling and layout.
+HTML: The structure of the web application.
+
+Project link:-
---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+function App() {
+  const [colour, setColour] = useState("cyan");
+
+  return (
+    <div
+      className="w-full h-screen flex items-start justify-center"
+      style={{ backgroundColor: colour }}
+    >
+      <div className="flex items-center justify-center p-4 rounded-2xl bg-slate-300 mt-5 gap-5 border-2 border-black">
+        <button
+          className="p-4 bg-red-600 text-white text-3xl rounded-2xl border-2 border-black"
+          onClick={() => {
+            setColour("red");
+          }}
+        >
+          Red
+        </button>
+        <button
+          className="p-4 bg-blue-600 text-white text-3xl rounded-2xl border-2 border-black"
+          onClick={() => {
+            setColour("blue");
+          }}
+        >
+          Blue
+        </button>
+        <button
+          className="p-4 bg-green-600 text-white text-3xl rounded-2xl border-2 border-black"
+          onClick={() => {
+            setColour("green");
+          }}
+        >
+          Green
+        </button>
+        <button
+          className="p-4 bg-pink-600 text-white text-3xl rounded-2xl border-2 border-black"
+          onClick={() => {
+            setColour("pink");
+          }}
+        >
+          Pink
+        </button>
+        <button
+          className="p-4 bg-white text-black text-3xl rounded-2xl border-2 border-black"
+          onClick={() => {
+            setColour("white");
+          }}
+        >
+          White
+        </button>
+        <button
+          className="p-4 bg-orange-600 text-white text-3xl rounded-2xl border-2 border-black"
+          onClick={() => {
+            setColour("orange");
+          }}
+        >
+          Orange
+        </button>
+        <button
+          className="p-4 bg-yellow-400 text-white text-3xl rounded-2xl border-2 border-black"
+          onClick={() => {
+            setColour("yellow");
+          }}
+        >
+          Yellow
+        </button>
+        <button
+          className="p-4 bg-purple-600 text-white text-3xl rounded-2xl border-2 border-black"
+          onClick={() => {
+            setColour("purple");
+          }}
+        >
+          Purple
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
