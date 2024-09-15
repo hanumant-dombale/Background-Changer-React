1f83934911cdce20334c544052e36f47f844ee0b
Hanumamt Dombale <hanumantdombale60@gmail.com>
2024-09-16 04:09:51 +0530

    Update file

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
 
   return (
     <div
-      className="w-full h-screen flex items-start justify-center"
+      className="w-[100%] h-screen flex items-start justify-center"
       style={{ backgroundColor: colour }}
     >
-      <div className="flex items-center justify-center p-4 rounded-2xl bg-slate-300 mt-5 gap-5 border-2 border-black">
+      <div className="flex flex-wrap items-center justify-center p-4 rounded-2xl bg-slate-300 m-5 gap-5 border-2 border-black">
         <button
           className="p-4 bg-red-600 text-white text-3xl rounded-2xl border-2 border-black"
           onClick={() => {
